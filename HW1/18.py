6ebbe9a4b91811a5e6327d0c40ef378e51de9622
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-09-25 14:42:10 -0600

    refactoring of 18

--- HW1/18.py
+++ HW1/18.py
@@ -3,6 +3,8 @@ import numpy as np
 
 x = np.linspace(5, 15)
 y = 8.0+(25.0-(x-10.0)**2.0)**0.5
-plt.plot(x, y, 'k')
-plt.plot(x, np.linspace(1, 1, len(x))*8, 'k')
+
+plt.title("18")
+plt.plot(x, y)
+plt.plot(x, np.linspace(1, 1, len(x))*8)
 plt.show()
