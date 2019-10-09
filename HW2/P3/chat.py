cbb07cff8bf8485fe02cccd94e570e73c6c2086d
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-10-08 20:02:20 -0600

    Removed fancy stuff, works as specified

--- HW2/P3/chat.py
+++ HW2/P3/chat.py
@@ -5,29 +5,15 @@ print("Connecting to the chat server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-def help():
-    print("/help: show this prompt\n/shutdown close the server")
-    print("/exit: disconnect chat client from server")
-
-user_name = input('Welcome to the chat client, enter name: ')
+user_name = input('Welcome to the chat client, Enter name: ')
 
 socket.send_string('New user {} has entered the channel'.format(user_name))
+# print("Type /help for commands")
 
-message_name = '{}'.format(user_name)
 while True:
-    message_string = input('Channel: ')
-
-    if message_string == '/help':
-        help()
-    elif message_string == '/exit':
-        socket.send_string('User {} has left the chat.'.format(user_name))
-        print("Disconnecting")
-        break
-    elif message_string == '/shutdown':
-        socket.send_string('Shutting down server')
-        print("Disconnecting")
-        break
-    else:
-        socket.send_string(message_name + message_string)
-
     message = socket.recv_string()
+    print(message)
+
+    message_string = input('Channel: ')
+    send_message = user_name + ":" + message_string
+    socket.send_string(send_message)
