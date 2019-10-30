10046d7c2d68477f439cda70573c38caa4658713
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-10-30 01:19:35 -0600

    proper spacing

--- HW4/P4.py
+++ HW4/P4.py
@@ -1,11 +1,21 @@
 import numpy as np 
 from scipy.signal import argrelextrema
 
+steps = 100.0
+
+v1min = 0.18
+v1max = 0.22
+v1space = (v1max - v1min)/steps
+
+v2min = 0.27
+v2max = 0.33
+v2space = (v2max-v2min)/steps
+
 f = 0.7
 b = 10.0
 
-v1 = np.arange(0.18, 0.22, 0.001, np.float)
-v2 = np.arange(0.27, 0.33, 0.001, np.float)
+v1 = np.arange(v1min, v1max, v1space, np.float)
+v2 = np.arange(v2min, v2max, v2space, np.float)
 
 g = (f*b)/(v1+v2)
 
