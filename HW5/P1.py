e987612c09be80ae268e49afc843fcb03b2c0ff2
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-11-26 06:08:31 -0700

    cleanup

--- HW5/P1.py
+++ HW5/P1.py
@@ -26,27 +26,26 @@ i= 0
 
 print(len(xpts))
 while(list_index<len(xpts) or list_index<len(ypts)):
-    i=0
     print("working on", xpts[list_index],ypts[list_index])
     while (i < N-1):
         th_err = atan2(ypts[list_index] - y[i], xpts[list_index] - x[i]) - th[i]
         d1 = abs(x[i] - xpts[list_index])
         d2 = abs(y[i] - ypts[list_index])
         w = v
-        d = sqrt((d1 * d1) + (d2 * d2))
+        d = sqrt(d1*d1+d2*d2)
         if (d<=dia):
             break
         w1 = w + k1 * th_err
         w2 = w - k1 * th_err
-        if (d < 5):
-            w1, w2 = k2 * d * (w + k1 * th_err), k2 * d * (w - k1 * th_err)
-        dx = (r * dt / 2.0) * (w1 + w2) * cos(th[i])
-        dy = (r * dt / 2.0) * (w1 + w2) * sin(th[i])
-        dth = (r * dt / (2.0 * l)) * (w1 - w2)
-        x[i + 1] = x[i] + dx
-        y[i + 1] = y[i] + dy
-        th[i + 1] = th[i] + dth
-        i = i + 1
+        if (d<5):
+            w1, w2 = k2*d*(w + k1*th_err), k2*d*(w - k1*th_err)
+        dx = (r*dt/2.0)*(w1+w2)*cos(th[i])
+        dy = (r*dt/2.0)*(w1+w2)*sin(th[i])
+        dth = (r*dt/(2.0*l))*(w1-w2)
+        x[i+1] = x[i] + dx
+        y[i+1] = y[i] + dy
+        th[i+1] = th[i] + dth
+        i = i+1
 
     list_index=list_index+1
 
